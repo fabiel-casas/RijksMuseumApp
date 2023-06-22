6fde21396c10461dcdca85e1c0eff0b31e385c58
Fabiel Casas <fabiel016_@hotmail.com>
2023-06-22 22:10:41 +0200

    Force dark theme color.

--- app/src/main/java/fabiel/casas/rijksmuseumapp/ui/theme/Theme.kt
+++ app/src/main/java/fabiel/casas/rijksmuseumapp/ui/theme/Theme.kt
@@ -50,18 +50,9 @@ private val DarkColorScheme = darkColorScheme(
 @Composable
 fun RijksMuseumAppTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-        darkTheme -> DarkColorScheme
-        else -> DarkColorScheme
-    }
+    val colorScheme = DarkColorScheme
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
