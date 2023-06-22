4e9024c3531f84b703a247fa8978f858c16accdf
Fabiel Casas <fabiel016_@hotmail.com>
2023-06-22 22:05:55 +0200

    First Commit
    - Add application theme.
    - Add application Font Family.

---
+++ app/src/main/java/fabiel/casas/rijksmuseumapp/ui/theme/RalewayFontFamily.kt
@@ -0,0 +1,107 @@
+package fabiel.casas.rijksmuseumapp.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import fabiel.casas.rijksmuseumapp.R
+
+/**
+ * RijksMuseumApp
+ * Created on 22/06/2023.
+ * Author: johan
+ */
+internal val RalewayFontFamily = FontFamily(
+    fonts = listOf(
+        Font(
+            resId = R.font.raleway_black,
+            weight = FontWeight.W900,
+            style = FontStyle.Normal
+        ),
+        Font(
+            resId = R.font.raleway_blackitalic,
+            weight = FontWeight.W900,
+            style = FontStyle.Italic
+        ),
+        Font(
+            resId = R.font.raleway_extrabold,
+            weight = FontWeight.W800,
+            style = FontStyle.Normal
+        ),
+        Font(
+            resId = R.font.raleway_extrabolditalic,
+            weight = FontWeight.W800,
+            style = FontStyle.Italic
+        ),
+        Font(
+            resId = R.font.raleway_bold,
+            weight = FontWeight.Bold,
+            style = FontStyle.Normal
+        ),
+        Font(
+            resId = R.font.raleway_bolditalic,
+            weight = FontWeight.Bold,
+            style = FontStyle.Italic
+        ),
+        Font(
+            resId = R.font.raleway_semibold,
+            weight = FontWeight.SemiBold,
+            style = FontStyle.Normal
+        ),
+        Font(
+            resId = R.font.raleway_semibolditalic,
+            weight = FontWeight.SemiBold,
+            style = FontStyle.Italic
+        ),
+        Font(
+            resId = R.font.raleway_medium,
+            weight = FontWeight.Medium,
+            style = FontStyle.Normal
+        ),
+        Font(
+            resId = R.font.raleway_mediumitalic,
+            weight = FontWeight.Medium,
+            style = FontStyle.Italic
+        ),
+        Font(
+            resId = R.font.raleway_regular,
+            weight = FontWeight.Normal,
+            style = FontStyle.Normal
+        ),
+        Font(
+            resId = R.font.raleway_regular,
+            weight = FontWeight.Normal,
+            style = FontStyle.Italic
+        ),
+        Font(
+            resId = R.font.raleway_light,
+            weight = FontWeight.Light,
+            style = FontStyle.Normal
+        ),
+        Font(
+            resId = R.font.raleway_lightitalic,
+            weight = FontWeight.Light,
+            style = FontStyle.Italic
+        ),
+        Font(
+            resId = R.font.raleway_extralight,
+            weight = FontWeight.ExtraLight,
+            style = FontStyle.Normal
+        ),
+        Font(
+            resId = R.font.raleway_extralightitalic,
+            weight = FontWeight.ExtraLight,
+            style = FontStyle.Italic
+        ),
+        Font(
+            resId = R.font.raleway_thin,
+            weight = FontWeight.Thin,
+            style = FontStyle.Normal
+        ),
+        Font(
+            resId = R.font.raleway_thinitalic,
+            weight = FontWeight.Thin,
+            style = FontStyle.Italic
+        ),
+    )
+)
