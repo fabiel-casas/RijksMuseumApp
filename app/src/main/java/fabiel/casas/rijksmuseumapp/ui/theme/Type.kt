dec05dfefc08193190d55bd4c1e6b48e6339368d
Fabiel Casas <fabiel016_@hotmail.com>
2023-06-23 09:50:51 +0200

    Update Typography and app icon.

--- app/src/main/java/fabiel/casas/rijksmuseumapp/ui/theme/Type.kt
+++ app/src/main/java/fabiel/casas/rijksmuseumapp/ui/theme/Type.kt
@@ -2,12 +2,68 @@ package fabiel.casas.rijksmuseumapp.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
 // Set of Material typography styles to start with
 val Typography = Typography(
+    displayLarge = TextStyle(
+        fontFamily = RalewayFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 30.sp,
+        lineHeight = 16.sp,
+    ),
+    displayMedium = TextStyle(
+        fontFamily = RalewayFontFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 30.sp,
+        lineHeight = 16.sp,
+    ),
+    displaySmall = TextStyle(
+        fontFamily = RalewayFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 30.sp,
+        lineHeight = 16.sp,
+    ),
+    headlineLarge = TextStyle(
+        fontFamily = RalewayFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 26.sp,
+        lineHeight = 16.sp,
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = RalewayFontFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 26.sp,
+        lineHeight = 16.sp,
+    ),
+    headlineSmall = TextStyle(
+        fontFamily = RalewayFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 26.sp,
+        lineHeight = 16.sp,
+    ),
+    titleLarge = TextStyle(
+        fontFamily = RalewayFontFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 22.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = RalewayFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 22.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.5.sp
+    ),
+    titleSmall = TextStyle(
+        fontFamily = RalewayFontFamily,
+        fontWeight = FontWeight.Light,
+        fontSize = 22.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.5.sp
+    ),
     bodyLarge = TextStyle(
         fontFamily = RalewayFontFamily,
         fontWeight = FontWeight.Normal,
@@ -15,18 +71,39 @@ val Typography = Typography(
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     ),
-    titleLarge = TextStyle(
+    bodyMedium = TextStyle(
         fontFamily = RalewayFontFamily,
         fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        fontSize = 16.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.5.sp
+    ),
+    bodySmall = TextStyle(
+        fontFamily = RalewayFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.5.sp
     ),
     labelSmall = TextStyle(
         fontFamily = RalewayFontFamily,
-        fontWeight = FontWeight.Medium,
+        fontWeight = FontWeight.Thin,
         fontSize = 11.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
-    )
+    ),
+    labelLarge = TextStyle(
+        fontFamily = RalewayFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 11.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.5.sp
+    ),
+    labelMedium = TextStyle(
+        fontFamily = RalewayFontFamily,
+        fontWeight = FontWeight.Light,
+        fontSize = 11.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.5.sp
+    ),
 )
